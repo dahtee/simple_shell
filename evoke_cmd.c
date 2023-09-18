fbc1caebf652e41f8214cd01f907873bc0df6b4a
dahtee <datibosa@gmail.com>
2023-09-18 12:03:59 -0700

    path int

--- evoke_cmd.c
+++ evoke_cmd.c
@@ -1,43 +1,61 @@
 #include "shell.h"
 
 /**
- * evoke_command - execute command that are parsed by user
- * @cmd: Users command inputted.
- *
- * Return: Nothing since is void.
+ * evoke_command - Execute a command and print its exit status.
+ * @cmd: The command to execute.
  */
 void evoke_command(const char *cmd)
 {
-	pid_t new_pid; /*child process or PID*/
-	int i; /*index for user arguments*/
+    pid_t new_pid;
+    char **usr_args;
+    int i;
 
-	new_pid = fork();
-	if (new_pid == 0)
-	{
-		char *usr_args[128]; /*array to handle user args added to commands*/
-		char *to_ken;
+    usr_args = malloc(128 * sizeof(char *));
+    if (usr_args == NULL)
+    {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
 
-		to_ken = strtok((char *)cmd, " ");
-		for (i = 0; to_ken != NULL; i++)
-		{
-			usr_args[i] = to_ken;
-			to_ken = strtok(NULL, " ");
-		}
-		usr_args[i] = NULL; /*if user argument is NULL terminate*/
+    new_pid = fork();
 
-		execvp(usr_args[0], usr_args); /*finally execute users argument*/
-		print_txt("Error executing your command\n");
-		exit(EXIT_FAILURE);
-	}
-	else if (new_pid == -1)
-	{
-		print_txt("Error encountered during forking\n");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		wait(NULL);
-		/** this section controls the parent process*/
-	}
+    if (new_pid < 0)
+    {
+        perror("fork failed");
+        free(usr_args);
+        exit(EXIT_FAILURE);
+    }
 
+    if (new_pid == 0)
+    {
+        if (execv("/bin/echo", usr_args) == -1)
+        {
+            perror("execv failed");
+            free(usr_args);
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        int status;
+        waitpid(new_pid, &status, 0);
+
+        if (WIFEXITED(status))
+        {
+            print_txt("Child exited with status %d\n", WEXITSTATUS(status));
+        }
+        else
+        {
+            print_txt("Child terminated abnormally\n");
+        }
+
+        free(usr_args);
+    }
 }
+
+int main()
+{
+    evoke_command("/bin/echo");
+    return 0;
+}
+
--- shell.h
+++ shell.h
@@ -11,6 +11,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 /**
  * This section it my custom prototypes.
